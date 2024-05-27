52b086c5ceb5f19a5bdcfa8da31995448b5ec7ae
Actonis <mathis.nagmar@gmail.com>
2024-05-27 19:42:02 +0200

    feat(vite): change from CRA to vite for react configuration

---
+++ vite.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+})
